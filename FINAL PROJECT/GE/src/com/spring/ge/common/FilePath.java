f1bfe5da7db1e7dd0cd88958657912c680cb0163
foretdhiver <foretdhiver@gmail.com>
2020-04-15 09:27:33 +0900

    remove webstom GE directory

--- FINAL PROJECT/GE/src/com/spring/ge/common/FilePath.java
+++
@@ -1,9 +0,0 @@
-package com.spring.ge.common;
-
-public class FilePath {
-
-	public static final String UPLOAD_FILE_PATH =
-			"C://Users//bitcamp//Desktop//Java142_Project_GE//WebContent//em_Image";
-	public static final String DOWNLOAD_FILE_PATH = "/";
-	
-}
